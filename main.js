ea1b23a3f111e941da93c8703489f194085137f5
GabyFSantos <gabyfonsa@gmail.com>
2022-10-21 17:15:36 -0500

    first comit

---
+++ main.js
@@ -0,0 +1,16 @@
+// variables
+
+// nombre = "Gaby"
+// let num = 10;
+//const num2 = 10;
+
+
+//console.log(nombre + num + num2);
+//console.log(`${nombre} ${num} ${num2}`);
+
+
+// sumar dos numeros 
+let num1=21;
+let num2=24;
+let num3=num1 + num2;
+console.log(num3);
